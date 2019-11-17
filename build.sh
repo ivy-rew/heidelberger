9b2d4a35c065931b2dc30bac2753717832895c3b
LordEagle <r.wermelinger@gmail.com>
2019-11-17 21:53:07 +0100

    build with preview of final result

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mvn clean verify
- 
-pandoc -o target/heidelberger.epub extract.html
+
+pandoc  --epub-chapter-level=2\
+ -f html\
+ -o target/heidelberger.epub target/extract.html
+
+ebook-viewer target/heidelberger.epub
