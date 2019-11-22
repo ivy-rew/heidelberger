49c5f1a9b5bd75c7375c48f6d0a081dd8046567d
LordEagle <r.wermelinger@gmail.com>
2019-11-22 21:39:41 +0100

    back and forth to md
    try to keep footnotes

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-mvn clean verify
+#mvn clean verify
 
-pandoc  --epub-chapter-level=2\
+pandoc --atx-headers\
  -f html\
- -o target/heidelberger.epub target/extract.html
+ -o target/heidelberger.md target/extract.html
+
+# poor mans approach to turn escaped (whitespace pre-fixed) footnotes into real ones
+sed -i "s/\s\{4,\}\[/\[/g" target/heidelberger.md
+
+pandoc --toc --epub-chapter-level=2\
+ -o target/heidelberger.epub target/heidelberger.md
+
 
 ebook-viewer target/heidelberger.epub
