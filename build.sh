de4ec01b6efafe882d38d20f60e76316d3cd6d6c
LordEagle <r.wermelinger@gmail.com>
2019-11-23 20:51:39 +0100

    footnotes as pop-up: hint for kindle

--- build.sh
+++ build.sh
@@ -10,8 +10,13 @@ pandoc --atx-headers\
 sed -i "s/\s\{4,\}\[/\[/g" target/heidelberger.md
 sed -i "s/\\\//g" target/heidelberger.md
 
+# https://reformedforum.org/wp-content/blogs.dir/1/files/2012/03/Heidelberger_Katechismus_1563.jpg
 pandoc --toc --epub-chapter-level=2\
+ --epub-cover-image=Heidelberger_Katechismus_1563.jpg\
  -o target/heidelberger.epub\
  bookMeta.md target/heidelberger.md
 
 ebook-viewer target/heidelberger.epub
+
+# kindle: use azw3 to show footnotes as popup:
+# ebook-convert target/heidelberger.epub target/heidelberger.azw3
